9db79bea74bc237928e3f335901fac39e4c2d4cb
tctcetc <75934737+tctcetc@users.noreply.github.com>
2021-09-03 12:31:24 +0300

    Add files via upload

---
+++ M1F1rstPr0/src/m1f1rstpr0/M1F1rstPr0.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m1f1rstpr0;
+
+/**
+ *
+ * @author pupil
+ */
+public class M1F1rstPr0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello JKTV20!");
+    }
+    
+}
